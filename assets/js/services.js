f58bab13524cd7ee2a5b0b507eb7c1aebb4b0608
Segebari <segebarifred@gmail.com>
2023-07-08 15:21:20 +0100

    update

--- assets/js/services.js
+++ assets/js/services.js
@@ -14,5 +14,30 @@ document.addEventListener("click", function(event) {
   }
 });
 
+const hireButton = document.getElementById("hire-btn");
+const jobsButton = document.getElementById("jobs-btn");
+const portfolioDisplay = document.getElementById("porfolio-section");
+const jobsDisplay = document.getElementById("jobs-section");
+const resultsnoDisplay = document.getElementById("results-no");
 
+jobsButton.addEventListener("click", function(event) {
+    jobsDisplay.style.display = "block"; // Adjust the display property as needed
+    portfolioDisplay.style.display = "none"; // Adjust the display property as needed
+    hireButton.style.color = "#8E8E8E"; // Adjust the color
+    jobsButton.style.color = "#fff"; // Adjust the color
+    resultsnoDisplay.style.display = "none"; // Adjust the display property as needed
+    hireButton.style.borderBottom = "none"; // Adjust the border
+    jobsButton.style.borderBottom = "1px solid #fff"; // Adjust the border
+
+})
+
+hireButton.addEventListener("click", function(event) {
+    portfolioDisplay.style.display = "block"; // Adjust the display property as needed
+    jobsDisplay.style.display = "none"; // Adjust the display property as needed
+    hireButton.style.color = "#fff"; // Adjust the color
+    jobsButton.style.color = "#8E8E8E"; // Adjust the color
+    resultsnoDisplay.style.display = "block"; // Adjust the display property as needed
+    hireButton.style.borderBottom = "1px solid #fff"; // Adjust the border
+    jobsButton.style.borderBottom = "none"; // Adjust the border
+})
 
